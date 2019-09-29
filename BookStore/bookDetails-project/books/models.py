c69384f940909bdcf3c95b147078fa4eafe62a17
Jessica Moreno <jessica@Jessicas-MacBook-Pro.local>
2019-09-29 12:28:48 -0400

    deleted folder

--- BookStore/bookDetails-project/books/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Book(models.Model):
-    # Images
-    image = models.ImageField(upload_to='images/')
-    # Summary
-    summary = models.CharField(max_length=200)
--- BookStore/bookDetails-project/books/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'books/home.html')
-
-def details(request):
-    return render(request, 'books/details.html')
