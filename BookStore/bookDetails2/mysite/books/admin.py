c76a52f6d6c998ae9d5c8be92fc4346a780d1616
Jessica Moreno <jessicamoreno@Jessicas-MacBook-Pro.local>
2019-10-14 17:34:02 -0400

    missing comments, price, author bio

--- BookStore/bookDetails2/mysite/books/admin.py
+++ BookStore/bookDetails2/mysite/books/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Author, Books
+from .models import Author, Books, Comment
 
 # # Register your models here.
 
 admin.site.register(Author)
 admin.site.register(Books)
+admin.site.register(Comment)
--- BookStore/bookDetails2/mysite/books/models.py
+++ BookStore/bookDetails2/mysite/books/models.py
@@ -5,16 +5,28 @@ from django.db import models
 
 class Author(models.Model):
     authorName = models.CharField(max_length=15)
-
+    authorBio = models.TextField(default="default")
+    
     def __str__(self):
         return self.authorName
 
 class Books(models.Model):
     bookName = models.CharField(max_length=200)
     bookSummary = models.TextField
-    genre = models.CharField(max_length=15)
+    bookRating = models.CharField(max_length=1, default='1') # may need to be an int
+    # do i need to include book price ? 
+    genre = models.CharField(max_length=20)
     cover = models.ImageField(default='default.jpg', upload_to='bookCovers')
+    publisherName = models.CharField(max_length=20, default="no name")
+    publisherDate = models.CharField(max_length=20, default='no date')
     authorName = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    bookComment = models.TextField(default="default")
+    bookName = models.ForeignKey(Books, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bookComment
 
 
 # class Bookname(models.Model):
